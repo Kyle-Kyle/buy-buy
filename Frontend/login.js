e765e6c0cfeb0826429a864b4e549db29721b061
m-Just <mjust.lkc@gmail.com>
2017-03-18 18:10:16 +0800

    Fix angular login problem

--- Frontend/login.js
+++ Frontend/login.js
@@ -1,21 +1,8 @@
-(function () {
-    'use strict';
-
-    angular
-        .module('app')
-        .controller('LoginController', LoginController);
-
-    function LoginController() {
-        var vm = this;
-        vm.login = login;
-
-        function login() {
-          $http.post("/users/login", {username: vm.username, password: vm.password})
-          .then(function(response) {
-              $scope.login_feedback = response.data;
-              $scope.login_error = response.error_message;
-          });
-        };
-    }
-
-})();
+angular.module('loginApp', [])
+    .controller('loginController', ['$scope', function($scope) {
+      $scope.submit = $http.post("/users/login")
+        .then(function(response) {
+          $scope.login_feedback = response.data;
+          $scope.login_error = response.error_message;
+      });
+    }]);
