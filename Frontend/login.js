3e265e980e814797c61bbaea42564006aa608cc0
Kyle ZENG <jkjh1jkjh1@gmail.com>
2017-03-19 15:25:15 +0800

    update

--- Frontend/login.js
+++ Frontend/login.js
@@ -1,8 +1,11 @@
 angular.module('loginApp', [])
-    .controller('loginController', ['$scope', function($scope) {
-      $scope.submit = $http.post("/users/login")
-        .then(function(response) {
-          $scope.login_feedback = response.data;
-          $scope.login_error = response.error_message;
-      });
-    }]);
+	.controller('loginController', function($scope, $http) {
+		alert('bind success!');
+		$scope.login = function(){$http.post("/users/login")
+			.then(function(response) {
+				$scope.login_feedback = response.data;
+				$scope.login_error = response.error_message;
+			});
+		}
+});
+console.log('load success');
