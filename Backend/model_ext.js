be847d7e199844559df29578b3206c57560c6fd8
Kyle ZENG <jkjh1jkjh1@gmail.com>
2017-03-19 13:09:15 +0800

    clean code

--- Backend/model_ext.js
+++ Backend/model_ext.js
@@ -1,24 +1,20 @@
 model = require('./model');
+
 User = model.User;
 Category = model.Category;
 Item = model.Item;
+Message  = model.Message;
+Follow = model.Follow;
+Transaction = model.Transaction;
+Comment = model.Comment;
 
-
-Item.prototype.add_comment = function(info, cb){ /*require detail err_msg*/
-    err_msg = 'Fail to add comment';
-    item = this;
-    if(!(typeof info.uid !== 'undefined' && info.uid) || !(typeof info.content!== 'undefined' && info.content))return cb({feedback: 'Failure', err_msg: err_msg});
-    User.get(info.uid, function(result){
-        if(result.feedback != 'Success')return cb({feedback: 'Failure', err_msg: err_msg});
-        comment = {uid: info.uid, username: result.user.username, content: escape_html(info.content), timestamp: +new Date()};
-        item.comments.push(comment);
-        item.save(function(err, item){
-            if(err)return cb({feedback: 'Failure', err_msg: err_msg});
-            return cb({feedback: 'Success', item: item});
-        }); 
-    })  
-}
+//User.prototype.update_buffer(mid, cb){
+//}
 
 module.exports.User = User;
 module.exports.Category = Category;
 module.exports.Item = Item;
+module.exports.Message = Message;
+module.exports.Follow = Follow;
+module.exports.Transaction = Transaction;
+module.exports.Comment = Comment;
