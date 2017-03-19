96ed7560a254eaf39b5c4ccc3afa2153536b04cb
m-Just <mjust.lkc@gmail.com>
2017-03-19 16:55:55 +0800

    Dynamic loading with angular

--- Backend/test_index.js
+++ Backend/test_index.js
@@ -6,11 +6,11 @@ module.exports = app;
 
 app.use(express.static(path.join(__dirname, '../Frontend')));
 
-/*app.get('/*.html', function(req, res) {
+app.get('/*.html', function(req, res) {
 	res.type('text/html');
 	console.log(path.join(__dirname, '../Frontend', req.url));
 	res.sendFile(path.join(__dirname, '../Frontend', req.url));
-});*/
+});
 
 app.listen(8081, function(){
     console.log('Buy-Buy is running on port', 8081);
