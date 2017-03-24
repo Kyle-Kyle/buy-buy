cf8c386362f6f7a5dbe75585d97aeb9bbe327846
m-Just <mjust.lkc@gmail.com>
2017-03-24 19:22:44 +0800

    update login

--- Frontend/login.js
+++ Frontend/login.js
@@ -1,7 +1,12 @@
 angular.module('indexApp')
-	.controller('loginController', function($scope, $http) {
-		$scope.login = function(){$http.post("/users/login")
+	.controller('LoginController', function($scope, $http) {
+		$scope.login = function() {
+			$http.post("/users/login", {
+				username: $scope.credential.username,
+				password: $scope.credential.password,
+			})
 			.then(function(response) {
+				console.log(response);
 				$scope.login_feedback = response.data;
 				$scope.login_error = response.error_message;
 			});
