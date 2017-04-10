ba9103b9b82892336fed312d6d88dd4f8091042a
m-Just <mjust.lkc@gmail.com>
2017-04-11 02:30:50 +0800

    update

--- Frontend/recommendController.js
+++ Frontend/recommendController.js
@@ -2,14 +2,21 @@ var recommendController = function($scope, $http) {
   var range = function(len) {
     return Array.apply(null, Array(len)).map(function (_, i) {return i;});
   }
-
+  $scope.get_condition = ["Like New", "Very Good", "Good", "Acceptable", "Bad"]
   $scope.get_recommendations = function() {
     $http.get("/recommends")
     .then(function(response) {
+      console.log(response)
+      /*
       $scope.items = response.data;
+      $scope.items.forEach(function(item) {
+        console.log($scope.$parent.get_formatted_time(item.open_timestamp))
+        item.post_time = "Posted on " + $scope.$parent.get_formatted_time(item.open_timestamp);
+      });
       var rowNum = Math.floor($scope.items.length / 3);
       $scope.rows = range(rowNum);
       console.log($scope.items)
+      */
     });
   };
 }
